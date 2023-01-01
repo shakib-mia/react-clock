b9a3d123cf7419b05063a361028742884aea4366
shakib-mia <smdshakibmia2001@gmail.com>
2023-01-01 15:50:32 +0600

    basic logic added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import './App.css';
 function App() {
   return (
     <>
-      <h1 className='text-[44px]'>hello</h1>
+      <div className='w-100 px-3 relative'>
+        <div className='h-[2px] w-100 bg-black my-3'></div>
+        <span className='inline-block w-[20px] h-[20px] bg-[red] absolute top-[-9px]' id="second"></span>
+      </div>
+      <br /><br /><br />
+      <div className='w-100 px-3 relative'>
+        <div className='h-[2px] w-100 bg-black my-3'></div>
+        <span className='inline-block w-[20px] h-[20px] bg-[red] absolute top-[-9px]' id="min"></span>
+      </div>
     </>
   );
 }
