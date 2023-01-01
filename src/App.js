90b8dbefadabf7224ba664d665b7740f5f87d7cf
shakib-mia <smdshakibmia2001@gmail.com>
2023-01-01 18:02:28 +0600

    minute bar animation added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
 
 function App() {
+
+  function formatAMPM(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+  }
+
+  const time = formatAMPM(new Date);
+  const hour = parseInt(time.split(":")[0])
+  
+  console.log(hour);
+
   return (
     <div className='bg-gray-600 h-screen flex items-center'>
+
       <div className="relative mx-auto bg-gray-300 rounded-full w-80 h-80" id='container'>
-        <div className="absolute bg-black w-[90%] h-0.5 top-0 bottom-0 left-0 right-0 m-auto border-0" id='bar'>
+        <div className="absolute bg-black w-[90%] h-0.5 top-0 bottom-0 left-0 right-0 m-auto border-0" id='secondBar'>
+          <div className="relative p-0 border-0 m-0">
+            <div className="absolute bg-gray-300 w-[40%] left-0 top-[-1px] right-0 h-2.5"></div>
+          </div>
+        </div>
+        <div className="absolute bg-black w-[80%] h-1 top-0 bottom-0 left-0 right-0 m-auto border-0" id='bar' style={{rotate: hour}}>
           <div className="relative p-0 border-0 m-0">
             <div className="absolute bg-gray-300 w-[40%] left-0 top-[-1px] right-0 h-2.5"></div>
           </div>
