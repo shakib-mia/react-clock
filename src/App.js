4a3503c7d77b0e831ab7a121fdd8457168d6e19d
shakib-mia <smdshakibmia2001@gmail.com>
2023-01-01 20:44:43 +0600

    second bar animation added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
@@ -13,21 +14,32 @@ function App() {
     return strTime;
   }
 
+  const [secNumber, setSecNumber] = useState(0)
+  const date = new Date()
+
   const time = formatAMPM(new Date);
   const hour = parseInt(time.split(":")[0])
-  
-  console.log(hour);
+  const min = parseInt(time.split(':')[1].split(" ")[0]);
+  const sec = date.getSeconds()
+  // const sec = new Date().getSeconds();
 
+  console.log(sec);
+  
   return (
     <div className='bg-gray-600 h-screen flex items-center'>
 
       <div className="relative mx-auto bg-gray-300 rounded-full w-80 h-80" id='container'>
-        <div className="absolute bg-black w-[90%] h-0.5 top-0 bottom-0 left-0 right-0 m-auto border-0" id='secondBar'>
+        <div className="absolute bg-black w-[90%] h-0.5 top-0 bottom-0 left-0 right-0 m-auto border-0" id='secondBar' style={{rotate: sec*6 + "deg"}}>
           <div className="relative p-0 border-0 m-0">
             <div className="absolute bg-gray-300 w-[40%] left-0 top-[-1px] right-0 h-2.5"></div>
           </div>
         </div>
-        <div className="absolute bg-black w-[80%] h-1 top-0 bottom-0 left-0 right-0 m-auto border-0" id='bar' style={{rotate: hour}}>
+        <div className="absolute bg-black w-[80%] h-1 top-0 bottom-0 left-0 right-0 m-auto border-0" id='minBar' style={{rotate: min * 6+ "deg"}}>
+          <div className="relative p-0 border-0 m-0">
+            <div className="absolute bg-gray-300 w-[40%] left-0 top-[-1px] right-0 h-2.5"></div>
+          </div>
+        </div>
+        <div className="absolute bg-black w-[70%] h-2 top-0 bottom-0 left-0 right-0 m-auto border-0" id='bar' style={{ rotate: hour*30+"deg" }} >
           <div className="relative p-0 border-0 m-0">
             <div className="absolute bg-gray-300 w-[40%] left-0 top-[-1px] right-0 h-2.5"></div>
           </div>
