a48e5d8f2adbe4dd8ca862e86c95e53baa9d382a
shakib-mia <smdshakibmia2001@gmail.com>
2023-01-01 16:04:45 +0600

    second rotation added

--- src/App.js
+++ src/App.js
@@ -2,17 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <>
-      <div className='w-100 px-3 relative'>
-        <div className='h-[2px] w-100 bg-black my-3'></div>
-        <span className='inline-block w-[20px] h-[20px] bg-[red] absolute top-[-9px]' id="second"></span>
+    <div className='bg-gray-600 h-screen flex items-center'>
+      <div className="relative mx-auto">
+        <div className="absolute bg-black w-10 h-1 left-0 right-0 m-auto" id='bar'></div>
       </div>
-      <br /><br /><br />
-      <div className='w-100 px-3 relative'>
-        <div className='h-[2px] w-100 bg-black my-3'></div>
-        <span className='inline-block w-[20px] h-[20px] bg-[red] absolute top-[-9px]' id="min"></span>
-      </div>
-    </>
+    </div>
   );
 }
 
